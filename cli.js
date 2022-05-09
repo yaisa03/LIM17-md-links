04f3752eca22fbe526d56624e4e7176a9aadd38c
yaisa03 <yaisamichellea@gmail.com>
2022-05-09 13:00:56 -0500

    test de las funciones en cli.js

--- cli.js
+++ cli.js
@@ -3,35 +3,40 @@
 // const mdLinks = require("./index.js");
 const fs = require('fs');
 const path = require('path');
-
+// path ingresado en la consola
 const filePathInputed = process.argv[2];
 //const filePath = path.resolve(filePathInputed);
 // ver si la ruta es absoluto o relativo y convertir ruta a absoluta
-const pathToAbsolute = (file) => {
+const pathToAbsolute = (file) => (path.isAbsolute(file)) ? file : path.resolve(file);
+const filePath = pathToAbsolute(filePathInputed);
+/* const pathToAbsolute = (file) => {
     if (path.isAbsolute(file)) {
+        console.log(file);
         return file;
     } else {
+        console.log(file);
         return path.resolve(file);
     }
-}
-const filePath = pathToAbsolute(filePathInputed);
+} */
 // ver si la ruta existe 
 const pathIsVAlid = (file) => fs.existsSync(file);
 // ver si es carpeta o archivo
 const checkIfDirectory = (file) => fs.statSync(file).isDirectory();
 // mostrar el contenido de la ruta enviada
-const printPathContent = (file) => {
+const printPathContent = (file) => (!checkIfDirectory(file)) ? printFile(file): printDirectoryFiles(file);
+/* const printPathContent = (file) => {
     if (!checkIfDirectory(file)) {
         return printFile(file);
     } else {
         return printDirectoryFiles(file);
     }
-}
+} */
 // leer y mostrar contenido de un archivo
-const printFile = (file) => {
+const printFile = (file) =>  fs.readFileSync(file).toString();
+/* const printFile = (file) => {
     const fileContent = fs.readFileSync(file);
     return fileContent.toString();
-};
+}; */
 // leer y mostrar contenido de carpetas
 const printDirectoryFiles = (dir) => {
     let filesArray = fs.readdirSync(dir);
@@ -50,7 +55,19 @@ const filePathWorking = (filePath) => {
         return printPathContent(filePath);
     } else {
         console.log('La ruta no existe');
+        return 'La ruta no existe';
     }
 }
 
 filePathWorking(filePath);
+
+
+module.exports = {
+    pathToAbsolute,
+    pathIsVAlid,
+    checkIfDirectory,
+    printPathContent,
+    printFile,
+    printDirectoryFiles,
+    filePathWorking
+};
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,19 +1,72 @@
 // const api = require('../index.js');
-const cli = require('../cli.js');
+const { pathToAbsolute, pathIsVAlid, checkIfDirectory, printPathContent,
+  printFile, printDirectoryFiles, filePathWorking } = require('../cli.js');
 
 
-describe('cli', () => {
 
-  it('should return', () => {
-    cli();
+describe('pathToAbsolute', () => {
+  it('Debería retornar la misma ruta', () => {
+    expect(pathToAbsolute('D:\\Laboratoria\\LIM17-md-links\\index.js'))
+    .toBe('D:\\Laboratoria\\LIM17-md-links\\index.js');
   });
-
+  it('Debería retornar la ruta convertida a absoluta', () => {
+    expect(pathToAbsolute('index.js')).toBe('D:\\Laboratoria\\LIM17-md-links\\index.js');
+  });
 });
 
-/* describe('api', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('pathIsVAlid', () => {
+  it('Deberia comprobar que la ruta es valida', () => {
+    expect(pathIsVAlid('D:\\Laboratoria\\LIM17-md-links\\index.js')).toBe(true);
+    expect(pathIsVAlid('index.js')).toBe(true);
   });
+  it('Deberia comprobar que la ruta no es valida', () => {
+    expect(pathIsVAlid('sub.js')).toBe(false);
+    expect(pathIsVAlid('D:\\Laboratoria\\LIM17-md-links\\sub.js')).toBe(false);
+  });
+});
 
-}); */
+describe('checkIfDirectory', () => {
+  it('Deberia comprobar que es un directorio', () => {
+    expect(checkIfDirectory('D:\\Laboratoria\\LIM17-md-links\\testDirThree')).toBe(true);
+    expect(checkIfDirectory('testDirThree')).toBe(true);
+  });
+  it('Deberia comprobar que es un archivo', () => {
+    expect(checkIfDirectory('D:\\Laboratoria\\LIM17-md-links\\index.js')).toBe(false);
+    expect(checkIfDirectory('index.js')).toBe(false);
+  });
+});
+
+describe('printPathContent,', () => {
+  it('Deberia imprimir contenido de un directorio', () => {
+    expect(printPathContent('D:\\Laboratoria\\LIM17-md-links\\testDirThree'))
+    .toStrictEqual(printDirectoryFiles('D:\\Laboratoria\\LIM17-md-links\\testDirThree'));
+  });
+  it('Deberia imprimir contenido de un archivo', () => {
+    expect(printPathContent('D:\\Laboratoria\\LIM17-md-links\\index.js'))
+    .toBe(printFile('D:\\Laboratoria\\LIM17-md-links\\index.js'));
+  });
+});
+
+describe('printFile', () => {
+  it('Deberia imprimir contenido del archivo', () => {
+    expect(typeof printFile('D:\\Laboratoria\\LIM17-md-links\\index.js')).toBe('string');
+  });
+  /* test del contenido del archivo*/
+});
+describe('printDirectoryFiles', () => {
+  it('Deberia imprimir archivos del directorio', () => {
+    expect(typeof printDirectoryFiles('D:\\Laboratoria\\LIM17-md-links\\testDirTwo')).toBe('object');
+  }); // deberia de ser un array
+  /* test del contenido del directorio*/
+});
+
+describe('filePathWorking', () => {
+  it('Deberia mostrar contenido del archivo/directorio', () => {
+    expect(filePathWorking('D:\\Laboratoria\\LIM17-md-links\\index.js'))
+    .toBe(printPathContent('D:\\Laboratoria\\LIM17-md-links\\index.js'));
+  });
+  it('Deberia mostrar un mensaje de error si la ruta no exite', () => {
+    expect(filePathWorking('D:\\Laboratoria\\LIM17-md-links\\sub.js')).toBe('La ruta no existe');
+    // expect(pathIsVAlid('D:\\Laboratoria\\LIM17-md-links\\sub.js')).toBe(false);
+  });
+});
